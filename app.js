3587c4ebadfd28ef3107d3c6f79d775189cd291e
mccalldan <drm8266@gmail.com>
2019-10-04 09:07:28 -0400

    app routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const express =  require("express");
+const express = require("express");
 
 // Setting up App
 
@@ -11,32 +11,90 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true
+});
 
 // Setting Models and Schema
 
-const articleSchema = new mongoose.Schema ({
+const articleSchema = {
   title: String,
   content: String
-});
+};
 
 const Article = mongoose.model("Article", articleSchema);
 
-// Express Routes
+// Requests Targeting All Articles
 
-app.get("/articles", function(req,res){
+app.route("/articles")
 
-  Article.find({}, function(err, foundArticles){
-      console.log(foundArticles);
+  .get(function(req, res) {
+
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+
+    });
+
+  })
+
+  .post(function(req, res) {
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article.")
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Deleted Articles");
+      } else {
+        res.send(err);
+      }
+    });
   });
 
+// Requests Targeting Specific Article
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+      if(foundArticle){
+        res.send(foundArticle);
+      } else {
+        res.send("No article match.");
+      }
+  });
 });
+
+// .post()
+//
+// .delete();
+
 
 // Server Setup
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server listening on 3000");
 });
